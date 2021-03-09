9b798b49ea6bfdd17ad16f7a0aa6a7d17e43dbcc
dgraczyk <dtgraczyk@gmail.com>
2021-03-09 21:17:05 +0100

    Add UnitTests for Products features

---
+++ tests/Core/Application.UnitTests/Features/Products/Commands/AddPriceToProductTests.cs
@@ -0,0 +1,121 @@
+﻿using Application.Contracts.Persistence;
+using Application.Exceptions;
+using Application.Features.Products.Commands;
+using Application.Features.Products.Commands.DTO;
+using AutoMapper;
+using Domain.Entities;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.UnitTests.Features.Products.Commands
+{
+    public class AddPriceToProductTests
+    {
+        private readonly Mock<IProductInShopRepository> productInShopRepository;
+        private readonly Mock<IProductRepository> productRepository;
+        private readonly Mock<IShopRepository> shopRepository;
+
+        public AddPriceToProductTests()
+        {
+            this.productInShopRepository = new Mock<IProductInShopRepository>();
+            this.productRepository = new Mock<IProductRepository>();
+            this.shopRepository = new Mock<IShopRepository>();
+        }
+
+        [Fact]
+        public void Handle_ShouldThrowNotFoundExceptionIfShopIdNotExist()
+        {   
+            shopRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(default(Shop));
+
+            var handler = this.CreateHandler();
+
+            Func<Task> func = async () => await handler.Handle(this.CreateValidCommand(), CancellationToken.None);
+
+            func.Should().Throw<NotFoundException>();
+
+            shopRepository.Verify(repo => repo.GetByIdAsync(It.IsAny<int>()), Times.Once);
+
+            productInShopRepository.Verify(repo => repo.AddAsync(It.IsAny<ProductInShop>()), Times.Never());
+            productInShopRepository.Verify(repo => repo.UpdateAsync(It.IsAny<ProductInShop>()), Times.Never());
+        }
+
+        [Fact]
+        public void Handle_ShouldThrowNotFoundExceptionIfProductIdNotExist()
+        {
+            shopRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(new Shop());
+            productRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(default(Product));
+
+            var handler = this.CreateHandler();
+
+            Func<Task> func = async () => await handler.Handle(this.CreateValidCommand(), CancellationToken.None);
+
+            func.Should().Throw<NotFoundException>();
+
+            productRepository.Verify(repo => repo.GetByIdAsync(It.IsAny<int>()), Times.Once);
+
+            productInShopRepository.Verify(repo => repo.AddAsync(It.IsAny<ProductInShop>()), Times.Never());
+            productInShopRepository.Verify(repo => repo.UpdateAsync(It.IsAny<ProductInShop>()), Times.Never());
+        }
+
+        [Fact]
+        public async void Handle_ShouldAddProductToShopIfNotExistAndNextAddPriceToProduct()
+        {
+            shopRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(new Shop());
+            productRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(this.CreateProduct());
+            productInShopRepository.Setup(x => x.GetProductInShop(It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(default(ProductInShop));
+
+            var handler = this.CreateHandler();
+
+            await handler.Handle(this.CreateValidCommand(), CancellationToken.None);
+                        
+            productInShopRepository.Verify(repo => repo.AddAsync(It.IsAny<ProductInShop>()), Times.Once());
+            productInShopRepository.Verify(repo => repo.UpdateAsync(It.IsAny<ProductInShop>()), Times.Once());
+        }
+
+        [Fact]
+        public async void Handle_ShouldNotAddProductToShopIfExistAndNextAddPriceToProduct()
+        {
+            shopRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(new Shop());
+            productRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(this.CreateProduct());
+            productInShopRepository.Setup(x => x.GetProductInShop(It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(new ProductInShop());
+
+            var handler = this.CreateHandler();
+
+            await handler.Handle(this.CreateValidCommand(), CancellationToken.None);
+
+            productInShopRepository.Verify(repo => repo.AddAsync(It.IsAny<ProductInShop>()), Times.Never());
+            productInShopRepository.Verify(repo => repo.UpdateAsync(It.IsAny<ProductInShop>()), Times.Once());
+        }
+
+        private AddPriceToProduct.Handler CreateHandler()
+        {
+            return new AddPriceToProduct.Handler(                
+                this.productRepository.Object,
+                this.shopRepository.Object,
+                this.productInShopRepository.Object,
+                new Mock<IMapper>().Object);
+        }
+
+        private AddPriceToProduct.AddPriceToProductCommand CreateValidCommand()
+        {
+            return new AddPriceToProduct.AddPriceToProductCommand
+            {
+                ProductId = 1,
+                ShopId = 1,
+                BasePrice = new BasePriceDto
+                {
+                    Price = 2
+                }
+            };
+        }
+
+        private Product CreateProduct()
+        {
+            return new Product("TestName", "TestVendor", null, null, 1);           
+        }
+    }
+}
--- tests/Core/Application.UnitTests/Features/Products/Commands/CreateProductTests.cs
+++ tests/Core/Application.UnitTests/Features/Products/Commands/CreateProductTests.cs
@@ -1,9 +1,10 @@
 ﻿using Application.Contracts.Persistence;
+using Application.Exceptions;
 using Application.Features.Products.Commands;
-using Application.UnitTests.Mocks;
+using Application.Features.Products.Commands.DTO;
+using AutoMapper;
 using Domain.Entities;
 using FluentAssertions;
-using FluentValidation;
 using Moq;
 using System;
 using System.Threading;
@@ -14,6 +15,125 @@ namespace Application.UnitTests.Features.Products.Commands
 {
     public class CreateProductTests
     {
-       
+        private readonly Mock<IProductInShopRepository> productInShopRepository;
+        private readonly Mock<IProductRepository> productRepository;
+        private readonly Mock<ICategoryRepository> categoryRepository;
+        private readonly Mock<IShopRepository> shopRepository;
+
+        public CreateProductTests()
+        {
+            this.productInShopRepository = new Mock<IProductInShopRepository>();
+            this.productRepository = new Mock<IProductRepository>();
+            this.categoryRepository = new Mock<ICategoryRepository>();
+            this.shopRepository = new Mock<IShopRepository>();
+        }
+
+
+        [Fact]
+        public async void Handle_ShouldCreateProductInShop()
+        {
+            categoryRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(new Category());
+            shopRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(new Shop());
+            productRepository.Setup(x => x.DoesProductExist(It.IsAny<Product>())).ReturnsAsync(false);
+            productInShopRepository.Setup(x => x.DoesProductExistInShop(It.IsAny<Product>(), It.IsAny<int>())).ReturnsAsync(false);
+            productInShopRepository.Setup(x => x.AddAsync(It.IsAny<ProductInShop>())).ReturnsAsync(new ProductInShop());
+
+            var handler = this.CreateHandler();
+
+            await handler.Handle(this.CreateValidProductCommand(), CancellationToken.None);
+
+            productInShopRepository.Verify(repo => repo.AddAsync(It.IsAny<ProductInShop>()), Times.Once());
+        }
+
+        [Fact]
+        public void Handle_ShouldThrowNotFoundExceptionIfCategoryIdNotExist()
+        {
+            categoryRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(default(Category));
+
+            var handler = this.CreateHandler();
+
+            Func<Task> func = async () => await handler.Handle(this.CreateValidProductCommand(), CancellationToken.None);
+
+            func.Should().Throw<NotFoundException>();
+
+            categoryRepository.Verify(repo => repo.GetByIdAsync(It.IsAny<int>()), Times.Once);
+            productInShopRepository.Verify(repo => repo.AddAsync(It.IsAny<ProductInShop>()), Times.Never());
+        }
+
+        [Fact]
+        public void Handle_ShouldThrowNotFoundExceptionIfShopIdNotExist()
+        {
+            categoryRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(new Category());
+            shopRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(default(Shop));
+
+            var handler = this.CreateHandler();
+
+            Func<Task> func = async () => await handler.Handle(this.CreateValidProductCommand(), CancellationToken.None);
+
+            func.Should().Throw<NotFoundException>();
+
+            shopRepository.Verify(repo => repo.GetByIdAsync(It.IsAny<int>()), Times.Once);
+            productInShopRepository.Verify(repo => repo.AddAsync(It.IsAny<ProductInShop>()), Times.Never());
+        }
+
+        [Fact]
+        public void Handle_ShouldThrowBadRequestExceptionIfProductAlreadyExist()
+        {
+            categoryRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(new Category());
+            shopRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(new Shop());
+            productRepository.Setup(x => x.DoesProductExist(It.IsAny<Product>())).ReturnsAsync(true);
+
+            var handler = this.CreateHandler();
+
+            Func<Task> func = async () => await handler.Handle(this.CreateValidProductCommand(), CancellationToken.None);
+
+            func.Should().Throw<BadRequestException>();
+
+            productRepository.Verify(repo => repo.DoesProductExist(It.IsAny<Product>()), Times.Once);
+            productInShopRepository.Verify(repo => repo.AddAsync(It.IsAny<ProductInShop>()), Times.Never());
+        }
+
+        [Fact]
+        public void Handle_ShouldThrowBadRequestExceptionIfProductAlreadyExistInShop()
+        {
+            categoryRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(new Category());
+            shopRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(new Shop());
+            productRepository.Setup(x => x.DoesProductExist(It.IsAny<Product>())).ReturnsAsync(false);
+            productInShopRepository.Setup(x => x.DoesProductExistInShop(It.IsAny<Product>(), It.IsAny<int>())).ReturnsAsync(true);
+
+            var handler = this.CreateHandler();
+
+            Func<Task> func = async () => await handler.Handle(this.CreateValidProductCommand(), CancellationToken.None);
+
+            func.Should().Throw<BadRequestException>();
+
+            productInShopRepository.Verify(repo => repo.DoesProductExistInShop(It.IsAny<Product>(), It.IsAny<int>()), Times.Once);
+            productInShopRepository.Verify(repo => repo.AddAsync(It.IsAny<ProductInShop>()), Times.Never());
+        }
+
+        private CreateProduct.Handler CreateHandler()
+        {
+            return new CreateProduct.Handler(
+                this.productInShopRepository.Object,
+                this.productRepository.Object,
+                this.shopRepository.Object,
+                this.categoryRepository.Object,
+                new Mock<IMapper>().Object);
+        }
+
+        private CreateProduct.CreateProductCommand CreateValidProductCommand()
+        {
+            return new CreateProduct.CreateProductCommand
+            {
+                Name = "TestName",
+                Vendor = "TestVendor",
+                CategoryId = 1,
+                ShopId = 1,
+                BasePrice = new BasePriceDto
+                {
+                    Price = 2
+                }
+            };
+        }
     }
 }
